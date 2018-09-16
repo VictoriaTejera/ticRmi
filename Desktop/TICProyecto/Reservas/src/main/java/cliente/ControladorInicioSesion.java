020ab04afadce2593e5f5494c6d32a1b88b5474a
StephanieMS <39383974+StephanieMS@users.noreply.github.com>
2018-09-16 14:30:22 -0300

    Cambio de pantallas en javafx solucionado
    Pantallas de la aplicación del cliente: Inicio, inicio de sesión y registro

---
+++ Desktop/TICProyecto/Reservas/src/main/java/cliente/ControladorInicioSesion.java
@@ -0,0 +1,25 @@
+package cliente;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+
+public class ControladorInicioSesion {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+
+    @FXML
+    void handleSubmitButtonAction(ActionEvent event) {
+    }
+
+    @FXML
+    void initialize() {
+    }
+}
---
+++ Desktop/TICProyecto/Reservas/src/main/java/cliente/ControladorRegistro.java
@@ -0,0 +1,28 @@
+package cliente;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+
+public class ControladorRegistro {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+
+    @FXML
+    void handleSubmitButtonAction(ActionEvent event) {
+    }
+
+    @FXML
+    void initialize() {
+
+
+    }
+
+}
