c87036cad6d5beddb1c16acd450c207a1eab2fed
VKunze <39528476+VKunze@users.noreply.github.com>
2018-09-17 13:10:00 -0300

    fgdfgd

--- Desktop/TICProyecto/Reservas/src/main/java/persistance/entidades/Usuario.java
+++ Desktop/TICProyecto/Reservas/src/main/java/persistance/entidades/Usuario.java
@@ -1,11 +1,18 @@
 package persistance.entidades;
 
+import javax.persistence.Entity;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+
+@Entity
+@Table
 public class Usuario {
-	
+	@Id
+	private Integer celular;
+	private String Id;
 	private String Nombre;
 	private String contrasena;
 	private Float rating;
-	private Integer celular;
 	private Integer cantRatings;
 	
 }
