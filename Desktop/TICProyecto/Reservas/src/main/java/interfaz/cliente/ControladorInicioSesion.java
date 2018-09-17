fb3845f8f4faf257f0351d3b6170e41d3d3d5b06
StephanieMS <39383974+StephanieMS@users.noreply.github.com>
2018-09-17 20:27:38 -0300

    Lista de restaurantes solucionado
    Falta crear clase a parte para la creación de la tabla

--- Desktop/TICProyecto/Reservas/src/main/java/interfaz/cliente/ControladorInicioSesion.java
+++ Desktop/TICProyecto/Reservas/src/main/java/interfaz/cliente/ControladorInicioSesion.java
@@ -1,9 +1,15 @@
 package interfaz.cliente;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 
 public class ControladorInicioSesion {
@@ -13,10 +19,19 @@ public class ControladorInicioSesion {
 
     @FXML
     private URL location;
-
+    
+    @FXML
+    private Button btnConfirmarInicioSesion;
 
     @FXML
-    void handleSubmitButtonAction(ActionEvent event) {
+    void handleSubmitButtonAction(ActionEvent event) throws IOException {
+    	Stage stage = null;
+		if (event.getSource() == btnConfirmarInicioSesion) {
+			stage = (Stage) btnConfirmarInicioSesion.getScene().getWindow();
+		} 
+		
+		stage.setScene(Main.getSceneTable());
+		stage.show();
     }
 
     @FXML
