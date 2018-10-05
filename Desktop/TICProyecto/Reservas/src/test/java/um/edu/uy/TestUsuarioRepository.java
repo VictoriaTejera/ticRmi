8f27b7a3457f79cb69854f50a12d2fe5dbe15f27
VKunze <39528476+VKunze@users.noreply.github.com>
2018-10-05 09:56:12 -0300

    Cambios 00

--- Desktop/TICProyecto/Reservas/src/test/java/um/edu/uy/TestUsuarioRepository.java
+++ Desktop/TICProyecto/Reservas/src/test/java/um/edu/uy/TestUsuarioRepository.java
@@ -2,19 +2,15 @@ package um.edu.uy;
 
 import static org.junit.Assert.*;
 
+import java.sql.SQLException;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import um.edu.uy.persistance.RestauranteMgr;
-import um.edu.uy.persistance.RestauranteRepository;
 import um.edu.uy.persistance.UsuarioMgr;
-import um.edu.uy.persistance.UsuarioRepository;
-import um.edu.uy.persistance.entidades.Restaurante;
 import um.edu.uy.persistance.entidades.Usuario;
 
 @SpringBootTest
