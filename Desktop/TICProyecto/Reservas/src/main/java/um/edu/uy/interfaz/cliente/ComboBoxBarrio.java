194f047f7117b2328da8189b8aa5b297112db0f5
StephanieMS <39383974+StephanieMS@users.noreply.github.com>
2018-10-08 18:55:56 -0300

    cbx

--- Desktop/TICProyecto/Reservas/src/main/java/um/edu/uy/interfaz/cliente/ComboBoxBarrio.java
+++ Desktop/TICProyecto/Reservas/src/main/java/um/edu/uy/interfaz/cliente/ComboBoxBarrio.java
@@ -5,9 +5,11 @@ import org.springframework.stereotype.Component;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.GridPane;
 import um.edu.uy.persistance.BarrioMgr;
 
 @Component
@@ -16,15 +18,31 @@ public class ComboBoxBarrio {
 	  BarrioMgr barrioMgr;
 	  
 	  ComboBox<String> cbx;
+	  
+	  Button button;
    
 	  public Scene getScene(){
     	ObservableList<String> items = FXCollections.observableArrayList();
     	items.addAll(barrioMgr.getBarrios());
     	cbx = new ComboBox<>(items);
-    	StackPane pane = new StackPane(cbx);
+    	button = new Button ("Aceptar");
+    	
+    	GridPane pane = new GridPane();
+    	pane.add(cbx, 3, 0);
+    	pane.add(button, 0, 3);
+    	
     	Scene scene = new Scene(pane, 250, 150);
     	return scene;
     }
+	
+	public boolean getEventButton(ActionEvent event) {
+		boolean result = false;
+		if (event.getSource() == button) {
+    		result = true;
+		} 
+		
+		return result;
+	}
 	  
 	public String getBarrio() {
 		String barrio = cbx.getValue();
