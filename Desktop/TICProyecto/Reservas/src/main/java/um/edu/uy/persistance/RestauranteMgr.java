8fc4805e9f3fab5f6d2a9fe6503ed8d230aaea0b
VKunze <39528476+VKunze@users.noreply.github.com>
2018-10-08 17:17:15 -0300

    blabla

--- Desktop/TICProyecto/Reservas/src/main/java/um/edu/uy/persistance/RestauranteMgr.java
+++ Desktop/TICProyecto/Reservas/src/main/java/um/edu/uy/persistance/RestauranteMgr.java
@@ -31,13 +31,13 @@ public class RestauranteMgr{
 		repository.save(res);
 	}
 	
-	public List<Restaurante> filtrarPorBarrio(String nombreBarrio) {
-		List<Restaurante> restaurantes = repository.filtrarPorBarrio(nombreBarrio);
+	public ObservableList<Restaurante> filtrarPorBarrio(String nombreBarrio) {
+		ObservableList<Restaurante> restaurantes = repository.filtrarPorBarrio(nombreBarrio);
 		return restaurantes;
 	}
 	
-	public List<Restaurante> filtrarPorComida(String tipoComida){
-		List<Restaurante> restaurantes = repository.filtrarPorComida(tipoComida);
+	public ObservableList<Restaurante> filtrarPorComida(String tipoComida){
+		ObservableList<Restaurante> restaurantes = repository.filtrarPorComida(tipoComida);
 		return restaurantes;
 	}
 
