e56ffd98198dceaa8239bd5553431f0117b07e0f
StephanieMS <39383974+StephanieMS@users.noreply.github.com>
2018-11-12 19:57:46 -0300

    fgd

--- Desktop/TICProyecto/Reservas/src/main/java/um/edu/uy/interfaz/cliente/ControladorReservar.java
+++ Desktop/TICProyecto/Reservas/src/main/java/um/edu/uy/interfaz/cliente/ControladorReservar.java
@@ -1,28 +1,20 @@
 package um.edu.uy.interfaz.cliente;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import um.edu.uy.persistance.ReservaMgr;
-import um.edu.uy.persistance.entidades.Reserva;
 
 @Component
-public class ControladorReservar implements ApplicationContextAware {
+public class ControladorReservar {
 
     @FXML
     private ResourceBundle resources;
@@ -47,27 +39,20 @@ public class ControladorReservar implements ApplicationContextAware {
     
     @Autowired
     ReservaMgr reservaMgr;
-    
-    private ApplicationContext applicationContext;
 
     @FXML
-    void handleButtonAction(ActionEvent event) throws IOException {
+    void handleButtonAction(ActionEvent event) {
     	Stage stage = null;
-		Parent root = null;
-		FXMLLoader fxmlLoader = new FXMLLoader();
-		stage = new Stage();
-		fxmlLoader.setControllerFactory(applicationContext::getBean);
-		
     	if (event.getSource() == btnReservar) {
-    		reservaMgr.save(controladorInicioSesion.getUsuario().getCelular(), controlador2.getRestaurante().getRUT(), Integer.parseInt(cantPersonas.getText()));
-    		stage = (Stage) btnReservar.getScene().getWindow();
-			root = fxmlLoader.load(ControladorInicioSesion.class.getResourceAsStream("MenuPrincipal.fxml"));
+    		try {
+				reservaMgr.save(controladorInicioSesion.getUsuario().getCelular(), controlador.restSeleccionado().getRUT(),
+						Integer.parseInt(cantPersonas.getText()));
+				stage = (Stage) btnReservar.getScene().getWindow();
+				stage.close();
+			}catch(NumberFormatException e) {
+				showAlert("Ingrese una cantidad de personas.");
+			}
     	}
-    	
-    	Scene scene = new Scene(root);
-		scene.getStylesheets().add(ControladorInicio.class.getResource("style.css").toExternalForm());
-		stage.setScene(scene);
-		stage.show();
     }
 
     @FXML
@@ -75,11 +60,15 @@ public class ControladorReservar implements ApplicationContextAware {
         assert btnReservar != null : "fx:id=\"btnReservar\" was not injected: check your FXML file 'Reservar.fxml'.";
         assert cantPersonas != null : "fx:id=\"cantPersonas\" was not injected: check your FXML file 'Reservar.fxml'.";
     }
+    
+	public static void showAlert(String title) {
+        javafx.scene.control.Alert alert = new javafx.scene.control.Alert(javafx.scene.control.Alert.AlertType.INFORMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(null);
+        alert.showAndWait();
+    }
 
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-		this.applicationContext = applicationContext;
-
-	}
     
 }
 
--- Desktop/TICProyecto/Reservas/src/main/java/um/edu/uy/interfaz/cliente/ControladorReservarDirecto.java
+++ Desktop/TICProyecto/Reservas/src/main/java/um/edu/uy/interfaz/cliente/ControladorReservarDirecto.java
@@ -1,31 +1,20 @@
 package um.edu.uy.interfaz.cliente;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-import um.edu.uy.interfaz.cliente.clasesAuxiliares.RestauranteAUX;
 import um.edu.uy.persistance.ReservaMgr;
-import um.edu.uy.persistance.entidades.Reserva;
-import um.edu.uy.persistance.entidades.Restaurante;
-import um.edu.uy.persistance.entidades.Usuario;
 
 @Component("ControladorReservarDirecto")
-public class ControladorReservarDirecto implements ApplicationContextAware {
+public class ControladorReservarDirecto {
 
 	@FXML
 	private ResourceBundle resources;
@@ -48,32 +37,18 @@ public class ControladorReservarDirecto implements ApplicationContextAware {
 	@Autowired
 	ReservaMgr reservaMgr;
 
-	private ApplicationContext applicationContext;
-
-//	private Restaurante restaurante;
-
 	@FXML
-	void handleButtonAction(ActionEvent event) throws IOException {
+	void handleButtonAction(ActionEvent event){
 		Stage stage = null;
-		Parent root = null;
-		FXMLLoader fxmlLoader = new FXMLLoader();
-		stage = new Stage();
-		fxmlLoader.setControllerFactory(applicationContext::getBean);
-		
 		if (event.getSource() == btnReservar) {
 			try {
 				reservaMgr.save(controladorInicioSesion.getUsuario().getCelular(), controladorListarRestaurantes.getRestaurante().getRUT(),
 						Integer.parseInt(cantPersonas.getText()));
 				stage = (Stage) btnReservar.getScene().getWindow();
-				root = fxmlLoader.load(ControladorInicioSesion.class.getResourceAsStream("MenuPrincipal.fxml"));
+				stage.close();
 			}catch(NumberFormatException e) {
 				showAlert("Ingrese una cantidad de personas.");
 			}
-
-	    	Scene scene = new Scene(root);
-			scene.getStylesheets().add(ControladorInicio.class.getResource("style.css").toExternalForm());
-			stage.setScene(scene);
-			stage.show();
 		}
 	}
 
@@ -82,12 +57,6 @@ public class ControladorReservarDirecto implements ApplicationContextAware {
 		assert btnReservar != null : "fx:id=\"btnReservar\" was not injected: check your FXML file 'Reservar.fxml'.";
 		assert cantPersonas != null : "fx:id=\"cantPersonas\" was not injected: check your FXML file 'Reservar.fxml'.";
 	}
-
-	
-
-//	public void setRestaurante(Restaurante restaurante) {
-//		this.restaurante = restaurante;
-//	}
 	
 	public static void showAlert(String title) {
         javafx.scene.control.Alert alert = new javafx.scene.control.Alert(javafx.scene.control.Alert.AlertType.INFORMATION);
@@ -97,9 +66,5 @@ public class ControladorReservarDirecto implements ApplicationContextAware {
         alert.showAndWait();
     }
 	
-	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-		this.applicationContext = applicationContext;
-
-	}
 
 }
