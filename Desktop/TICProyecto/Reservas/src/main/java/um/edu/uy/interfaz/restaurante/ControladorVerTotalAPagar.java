7f4f5198d805ab485eb6bc7ec2867da7a60188e9
StephanieMS <39383974+StephanieMS@users.noreply.github.com>
2018-11-17 16:57:47 -0300

    merge arreglado

--- Desktop/TICProyecto/Reservas/src/main/java/um/edu/uy/interfaz/restaurante/ControladorVerTotalAPagar.java
+++ Desktop/TICProyecto/Reservas/src/main/java/um/edu/uy/interfaz/restaurante/ControladorVerTotalAPagar.java
@@ -66,8 +66,9 @@ public class ControladorVerTotalAPagar implements ApplicationContextAware {
 			stage = (Stage) btnVolver.getScene().getWindow();
 			root = fxmlLoader.load(AdminControladorRegistro.class.getResourceAsStream("MenuPrincipalRest.fxml"));
 		}
-<<<<<<< HEAD
-		stage.setScene(new Scene(root));
+		Scene scene = new Scene(root);
+		scene.getStylesheets().add(ControladorInicio.class.getResource("style.css").toExternalForm());
+		stage.setScene(scene);
 		stage.show();
 	}
 
@@ -84,27 +85,6 @@ public class ControladorVerTotalAPagar implements ApplicationContextAware {
 		txtFechaDesde.setText(controladorFechas.getFechaInicio());
 		txtFechaHasta.setText(controladorFechas.getFechaFin());
 	}
-=======
-		Scene scene = new Scene(root);
-		scene.getStylesheets().add(ControladorInicio.class.getResource("style.css").toExternalForm());
-		stage.setScene(scene);
-    	stage.show();
-    }
-
-    @FXML
-    void initialize() {
-        assert btnVolver != null : "fx:id=\"btnVolver\" was not injected: check your FXML file 'VerTotalAPagar.fxml'.";
-        assert txtCantidad != null : "fx:id=\"txtCantidad\" was not injected: check your FXML file 'VerTotalAPagar.fxml'.";
-        assert txtFechaDesde != null : "fx:id=\"txtFechaDesde\" was not injected: check your FXML file 'VerTotalAPagar.fxml'.";
-        assert txtFechaHasta != null : "fx:id=\"txtFechaHasta\" was not injected: check your FXML file 'VerTotalAPagar.fxml'.";
-
-        Restaurante rest=restauranteMgr.find(controlador.getRutRestaurante());
-        
-//        txtCantidad.setText(rest.);
-        txtFechaDesde.setText(controladorFechas.getFechaInicio());
-        txtFechaHasta.setText(controladorFechas.getFechaFin());
-    }
->>>>>>> 0214fdfdc67ee7c98f60dbc411583c17edc2e5ce
 
 	@Override
 	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
